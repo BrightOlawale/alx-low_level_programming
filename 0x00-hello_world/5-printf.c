105b43c6da48d26035925da0cb7589109bc77d58
BrightOlawale <oolatunjibright@yahoo.com>
2022-03-11 07:20:59 -0800

    C task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: 0 if exited properly, non-zero otherwise
+*/
+
+int main(void)
+{
+	printf("\"with proper grammer, but the output is a piece of art");
+	return (0);
+}
