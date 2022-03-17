fa1fa04159f93be61a7197a25554422813b98e95
BrightOlawale <oolatunjibright@yahoo.com>
2022-03-17 07:05:38 -0700

    commit 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print alphabets 10 times
+* Return: 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int times = 0;
+	char letter = 'a';
+
+	while (times < 10, times++)
+	{
+		letter = 'a';
+		while (letter < 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n')
+	}
+}
+
