0012fb0190f7fc46df2df7441ae94044c9845d5e
BrightOlawale <oolatunjibright@yahoo.com>
2022-03-17 05:51:54 -0700

    Commit 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ * Return: Void
+*/
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar("\n");
+}
