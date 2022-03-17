7e234e633cb6203fb12a406191bab890b4118715
BrightOlawale <oolatunjibright@yahoo.com>
2022-03-17 05:56:16 -0700

    Commit 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,9 +9,10 @@ void print_alphabet(void)
 {
 	char letter = 'a';
 
-	while (letter <= 'z'; letter++)
+	while (letter <= 'z')
 	{
 		_putchar(letter);
+		letter++
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
