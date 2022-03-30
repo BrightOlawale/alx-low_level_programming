1eccd5874712a28f599972c13164f76ec5ab70f2
BrightOlawale <oolatunjibright@yahoo.com>
2022-03-30 16:43:28 -0700

    commit 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,7 @@
 #define _MAIN_H_
 
 void _puts_recusion(char *s);
-void _print_rev_recursion(chr *s);
+void _print_rev_recursion(char *s);
 void _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
