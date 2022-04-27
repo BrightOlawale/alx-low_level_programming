262c4abc696a9dbafa7b6bf1ac2d2031227d7980
BrightOlawale <oolatunjibright@yahoo.com>
2022-04-27 00:52:57 +0000

    new node at end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (nextnode == NULL);
 		return (NULL);
 	if (*head == NULL)
-		nextnode->next = NULL;
+		*head = nextnode;
+		return (*head);
 
 	temp = *head;
 	while (temp->next != NULL)
