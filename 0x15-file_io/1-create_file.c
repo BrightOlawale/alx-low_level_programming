eb0017ab2ff99c1a269ec1eb7eade038a2f7ae3d
BrightOlawale <oolatunjibright@yahoo.com>
2022-05-02 22:44:28 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,6 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		printf("File cannot be created/written");
 		return (-1);
 	}
 	for (i = 0; text_content[i] == '\0'; i++)
