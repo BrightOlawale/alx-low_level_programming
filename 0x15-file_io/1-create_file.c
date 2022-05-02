ebc50898a8dc4af2d942b7ff5bb67953df713b3e
BrightOlawale <oolatunjibright@yahoo.com>
2022-05-02 22:47:58 +0000

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	for (i = 0; text_content[i] == '\0'; i++)
+	for (i = 0; text_content[i] != '\0'; i++)
 	{
 		written = write(fd, &text_content[i], 1);
 		if (written == -1)
@@ -28,4 +28,4 @@ int create_file(const char *filename, char *text_content)
 	}
 	close(fd);
 	return (1);
-}
+int create_file(const char *filename, char *text_content);}
