72099dd0c871054339f1ffa6922139fda36bafc0
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-10 16:56:07 +0000

    Insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		if (*h == NULL)
+		{
 			*h = new_node;
+		}
 		else
 		{
 			new_node->next = tmp;
@@ -36,7 +38,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (counter + 1 == idx)
 		{
 			if (tmp->next != NULL)
+			{
 				tmp->next->prev = new_node;
+			}
 			new_node->next = tmp->next;
 			tmp->next = new_node;
 			new_node->prev = tmp;
