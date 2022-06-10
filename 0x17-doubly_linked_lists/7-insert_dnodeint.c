fa981fcdd316f7ccfae204ee7802a404782f2f39
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-10 16:50:29 +0000

    Insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int index = 0;
 	dlistint_t *newnode;
 
-	if (h == NULL)
+	if (h == NULL || (*h == NULL && idx != 0))
 		return (NULL);
 	newnode = do_node(n);
 	if (!newnode)
