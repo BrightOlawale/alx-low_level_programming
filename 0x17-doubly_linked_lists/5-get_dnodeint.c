139debdc67d6526ea519313a4e34087232a8bab5
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-10 14:49:20 +0000

    Get node index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,7 +6,7 @@
  * @index: Node index to return.
  * Return: If found the node at the index.
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	int idx = 0;
 
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 		return (NULL);
 	while (idx != index)
 	{
-		head = head->next
+		head = head->next;
 		idx++;
 	}
 	return (head);
