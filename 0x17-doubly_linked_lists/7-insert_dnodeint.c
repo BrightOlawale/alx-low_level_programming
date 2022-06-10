5b522419367753de74a6474a6d37a0949d3fc810
BrightOlawale <oolatunjibright@yahoo.com>
2022-06-10 21:58:37 +0000

    Insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - Inserts a node at a given position.
  * @h: Pointer to a pointer to the first node on a linked list.
@@ -22,9 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		if (*h == NULL)
-		{
 			*h = new_node;
-		}
 		else
 		{
 			new_node->next = tmp;
@@ -38,9 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (counter + 1 == idx)
 		{
 			if (tmp->next != NULL)
-			{
 				tmp->next->prev = new_node;
-			}
 			new_node->next = tmp->next;
 			tmp->next = new_node;
 			new_node->prev = tmp;
@@ -50,4 +45,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	free(new_node);
 	return (NULL);
+}
 }
