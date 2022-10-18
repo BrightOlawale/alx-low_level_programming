8117cd9a7a7e9ced7eba8696412005e9c0abbb9c
Bright Olawale <oolatunjibright@yahoo.com>
2022-10-18 20:57:07 +0100

    passed advance test

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,34 +1,50 @@
-#include "search_algos.h"
 #include <math.h>
+#include "search_algos.h"
+
 /**
- * jump_search - incremmently goes through array then linearly
- * @array: list of integers being searched
- * @size: size of array
- * @value: number being matched
- * Return: index of matched element or -1
+ * jump_search - search a sorted array for a value using jump search method
+ * @array: the array to be searched
+ * @size: the size of the array (number of elements)
+ * @value: the int to be searched for
+ *
+ * Return: 1st index value is found at or -1 if not present or null array
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i;
-	int jump;
+	int jump = sqrt(size);
+	int start = 0;
+	int section_end = 0;
 
-	if (array == NULL) /* argument check */
+	if (array == NULL || size == 0 || array[start] > value)
 		return (-1);
 
-	jump = (int) sqrt(size); /* skip interval */
-
-	for (i = 0; i < size; i += jump) /* start off skipping */
+	while (section_end < (int)size)
 	{
-		if (array[i] == value)
-			return (i);
-		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
-		if (array[i] > value) /*reset params to preform linear search*/
+		if (array[section_end] < value)
 		{
-			size = i;  /* know the value is less than index i */
-			i -= jump; /* jump one level back */
-			jump = 1; /*linear search */
-			printf("Value found between indexes [%lu] and [%lu]\n", i, size);
+			printf("Value checked array[%d] = [%d]\n", section_end, array[section_end]);
+
+			start = section_end;
+			section_end += jump;
+		}
+		else
+		{
+			break;
 		}
 	}
-	return (-1); /* no match found */
+
+	printf("Value found between indexes [%d] and [%d]\n", start, section_end);
+
+	if (section_end > (int)size - 1)
+		section_end = size - 1;
+
+	while (start <= section_end)
+	{
+		printf("Value checked array[%d] = [%d]\n", start, array[start]);
+		if (array[start] == value)
+			return (start);
+		start++;
+	}
+
+	return (-1);
 }
--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,61 +1,48 @@
 #include "search_algos.h"
-/**
- * compare - compares 2 midbers
- * @mid: middle number
- * @value: midber being matched
- * Return: -1 left, 1 right, 0 match
- */
-int compare(int mid, int value)
-{
-	if (mid > value) /* search left */
-		return (-1);
-	if (mid < value) /* search right */
-		return (1);
-	return (0); /* match found */
-}
 
 /**
- * interpolation_search - itterately search for number
- * @array: list of integers being searched
- * @size: size of array
- * @value: number being matched
- * Return: index of matched element or -1
+ * interpolation_search - search for a value in a sorted array using
+ * interpolation search
+ * @array: pointer to the start of the array
+ * @size: the size of the array (number of elements)
+ * @value: the value to be searched for
+ *
+ * Return: first index value is found at or -1 if not present or array is null
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	int mid;
-	int left = 0;
-	int right = size - 1;
+	size_t pos;
+	int low = 0;
+	int high = size - 1;
 
-	while (array != NULL && left <= right)
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (low <= high)
 	{
-		mid = /* Find relative position */
-		(
-			left + /* use to determain relative index */
-			((right - left) / (array[right] - array[left]))
-			* (value - array[left]) /* lean left or right */
-		);
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
+			     * (value - array[low]));
 
-		/* if index is out of range break */
-		if (array[left] > value || value > array[right])
+		if (pos > size - 1)
 		{
-			printf("Value checked array[%i] is out of range\n", mid);
-			break;
+			printf("Value checked array[%ld] is out of range\n", pos);
+			return (-1);
 		}
 
-		printf("Value checked array[%i] = [%i]\n", mid, array[mid]);
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 
-		switch (compare(array[mid], value))
+		if (array[pos] == value)
 		{
-			case -1: /* search left */
-				right = mid - 1;
-				break;
-			case 1: /* search right */
-				left = mid + 1;
-				break;
-			case 0: /* match found */
-				return (mid);
+			return (pos);
+		}
+		else
+		{
+			if (array[pos] > value)
+				high = pos - 1;
+			else
+				low = pos + 1;
 		}
 	}
-	return (-1);  /* array NULL or no match found */
+
+	return (-1);
 }
